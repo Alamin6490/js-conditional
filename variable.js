d3a1f3131cb8dad59c8dadaf94ab8662a35e3472
Alamin6490 <132498750+Alamin6490@users.noreply.github.com>
2024-10-09 23:19:46 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,29 @@
+/** 
+ *VARIABLE ==> container
+ */
+
+ // number 
+ var sunglassPrice = 80;
+
+ //string 
+ var subject = "EEE";
+
+ //boolean
+ var passed = true;
+
+ /**
+  * NAMING CONVENTION
+  * 1. Single word
+  * 2. no quote
+  * 3. no gap or dash
+  * 4. can not start with number
+  * 5. prefer camelcase
+  */
+ /**
+  * OPERATION
+  * 1.+,-,*,/,%
+  * 2.+=,-+,*=,/=
+  * 3. isNaN
+  * 4.null
+  * 5.number
+  */
