32ee0ce8f2865c374a3e975c3f0d5a26e0a50311
Alamin6490 <132498750+Alamin6490@users.noreply.github.com>
2024-10-10 02:20:09 +0600

    added code

---
+++ comparision.js
@@ -0,0 +1,41 @@
+/**
+ * COMPARISON
+ * 1.BIGGER: >
+ * 2. less : <
+ * 3. equal : ==
+ * 4. greater than or equal: >=
+ * 5. less than or equal : <=
+ * 3.not equal : !=
+ * 7. and : &&
+ * 8. or :||
+ */
+
+//console.log(5>10);
+//console.log(5<10);
+//console.log(5==10);
+//console.log(10==10);
+//console.log(15>=10);
+//console.log(5<=10);
+//console.log(10<=10);
+//console.log(500<=10);
+
+
+var peyaraPrice = 40;
+var applePrice =200;
+
+console.log(peyaraPrice>applePrice)
+console.log(applePrice<peyaraPrice);
+
+
+/**
+ * --
+ * ===
+ */
+console.log(10 =='10')
+console.log(10 === '10')
+
+console.log(120 === 'tomato')
+
+//-----------
+console.log(10 != '10')
+console.log(10 !== '10')
---
+++ ifcondition.js
@@ -0,0 +1,43 @@
+/**
+ * Tui jodi amake khawas
+ * taile ami asbo
+ * 
+ * --------------
+ * if you buy food for  me 
+ *   i will come
+ * 
+ * ---------------
+ * if briyani price less than 300 taka
+ *       i will buy
+ * 
+ * 
+ * ------STRUCTURE-------
+ * if (condition) {
+ * }
+ */
+if(3 < 10){
+    console.log('i have a smaller value');
+}
+//example
+
+var briyaniPrice = 200;
+
+if(briyaniPrice <300){
+    console.log('mama give me a some briyani')
+}
+/**
+ * if(condition){
+ *    excute code if condition above is true 
+ * }
+ * else{
+ *    execute code if the condition is false
+ * }
+ */
+var weight = 20;
+if(weight < 20){
+    console.log('I will carry it my self');
+
+}
+else{
+    console.log('I will rent a rickshow')
+}
