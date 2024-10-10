8d5d2c8effb3eef3833778fe3e72542aa106652a
Alamin6490 <132498750+Alamin6490@users.noreply.github.com>
2024-10-10 10:43:30 +0600

    added code

---
+++ multiplecondition.js
@@ -0,0 +1,48 @@
+const salary = 20000;
+const isBcs = true;
+const height = 78;
+const hasCar = false;
+
+if(salary > 20000 && height > 66){
+    console.log('su-----patro');
+}
+else{
+    console.log('onno patro khuji')
+}
+//
+//if(salary >25000 || height > 72){
+    console.log('eso baba kobul');
+}
+else{
+    console.log('vaag tui mofij')
+}
+
+// more and more condition
+if(salary >25000 || height > 72 || isBcs == true){
+    console.log('eso baba kobul');
+}
+else{
+    console.log('vaag tui mofij')
+}
+
+// && condition everthing fullfill
+if(salary >25000 && height > 72 && isBcs == true){
+    console.log('eso baba kobul');
+}
+else{
+    console.log('vaag tui mofij')
+}
+
+// complex condition
+if( (salary >25000 && hascar == true) ||   isBcs == true){
+    console.log('eso baba kobul');
+}
+else{
+    console.log('vaag tui mofij')
+}
+// more example
+if( (salary >25000 || hascar == true) &&   isBcs == true){
+    console.log('eso baba kobul');
+}
+else{
+    console.log('vaag tui mofij')
