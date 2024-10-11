29ad8c22105cc4d2c4806916e1e55a7e18da5d26
Alamin6490 <132498750+Alamin6490@users.noreply.github.com>
2024-10-11 11:02:31 +0600

    part-3

---
+++ nestedif-elsecondition.js
@@ -0,0 +1,17 @@
+const money = 200;
+if(money > 300){
+    console.log('are you rich!!');
+}
+else{
+    if(money > 100){
+        console.log('tui gorib o nah borolok o na');
+    }
+    else{
+        if(money > 0){
+            console.log('dusto kola kha calcium bara');
+        }
+        else{
+            console.log('tui amar bondho nah');
+        }
+    }
+}
