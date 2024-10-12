bac7e91cbed0c1b455220b3979e7299f83c6d54d
Alamin6490 <132498750+Alamin6490@users.noreply.github.com>
2024-10-12 08:24:42 +0600

    part8

---
+++ advanced-Logical-Not-Operator.js
@@ -0,0 +1,17 @@
+const isLeader = true;
+if(isLeader){
+    console.log('leader asche chair charo')
+    }
+    else{
+        console.log('vitore dukte dibo na');
+
+    }
+
+    const isPassed = false;
+    // if(isPassed === false)
+    if(!isPassed){
+        console.log('toke biye diya dibo')
+    }
+    else{
+        console.log('tumi amader noyon rmoni')
+    }
